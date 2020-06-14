2ff60650187764eefe854ac789e1f13d2ead6f32
mieubrisse <mieubrisse@gmail.com>
2020-06-14 18:29:14 -0300

    Slowwwly making progress towards a dependency-feeding test

--- commons/networks/service_network_config_test.go
+++ commons/networks/service_network_config_test.go
@@ -8,17 +8,44 @@ import (
 	"time"
 )
 
+// ======================== Test Service ========================
+type TestService struct {
+	result string
+}
+func (service TestService) GetTestEndpointResult() string {
+	return service.result
+}
 
-type TestService struct {}
 
 // ======================== Test Initializer Core ========================
-type TestInitializerCore struct {}
+type TestInitializerCore struct {
+	lastDependencyResults map[string]bool
+	serviceResult string
+}
+
+func NewTestInitializerCore(lastDependencyResults map[string]bool, serviceResult string) *TestInitializerCore {
+	return &TestInitializerCore{lastDependencyResults: lastDependencyResults, serviceResult: serviceResult}
+}
+
+
+
 func (t TestInitializerCore) GetUsedPorts() map[int]bool {
 	return make(map[int]bool)
 }
 
 func (t TestInitializerCore) GetStartCommand(publicIpAddr string, dependencies []services.Service) ([]string, error) {
-	return make([]string, 0), nil
+	castedServices := make([]TestService, 0, len(dependencies))
+	for _, service := range dependencies {
+		castedServices = append(castedServices, service.(TestService))
+	}
+
+	dependencyResults := make(map[string]bool)
+	for _, service := range castedServices {
+		dependencyResults[service.GetTestEndpointResult()] = true
+	}
+
+	t.lastDependencyResults = dependencyResults
+	return make([]string, 0, 0), nil
 }
 
 func (t TestInitializerCore) GetServiceFromIp(ipAddr string) services.Service {
@@ -34,8 +61,14 @@ func (t TestInitializerCore) InitializeMountedFiles(filepathsToMount map[string]
 	return nil
 }
 
-func getTestInitializerCore() services.ServiceInitializerCore {
-	return TestInitializerCore{}
+// Returns a set of the results of calling "GetTestEndpointResult" on each dependency service
+func (t TestInitializerCore) GetLastDependencyResults() map[string]bool {
+	return t.lastDependencyResults
+}
+
+// The serviceResult is the string that a service constructed with this InitializerCore will return
+func getTestInitializerCore(serviceResult string) *TestInitializerCore {
+	return NewTestInitializerCore(make(map[string]bool), serviceResult)
 }
 
 
@@ -62,7 +95,7 @@ func TestDisallowingNonexistentConfigs(t *testing.T) {
 
 func TestDisallowingNonexistentDependencies(t *testing.T) {
 	builder := NewServiceNetworkConfigBuilder()
-	config := builder.AddTestImageConfiguration(getTestInitializerCore(), getTestCheckerCore())
+	config := builder.AddTestImageConfiguration(getTestInitializerCore("test"), getTestCheckerCore())
 
 	dependencies := map[int]bool{
 		0: true,
@@ -79,22 +112,22 @@ func TestConfigurationIdsDifferent(t *testing.T) {
 	testImage := "testImage"
 	idSet := make(map[int]bool)
 	builder := NewServiceNetworkConfigBuilder()
-	config1 := builder.AddTestImageConfiguration(getTestInitializerCore(), getTestCheckerCore())
+	config1 := builder.AddTestImageConfiguration(getTestInitializerCore("test1"), getTestCheckerCore())
 	idSet[config1] = true
-	config2 := builder.AddStaticImageConfiguration(&testImage, getTestInitializerCore(), getTestCheckerCore())
+	config2 := builder.AddStaticImageConfiguration(&testImage, getTestInitializerCore("test2"), getTestCheckerCore())
 	idSet[config2] = true
-	config3 := builder.AddTestImageConfiguration(getTestInitializerCore(), getTestCheckerCore())
+	config3 := builder.AddTestImageConfiguration(getTestInitializerCore("test3"), getTestCheckerCore())
 	idSet[config3] = true
-	config4 := builder.AddStaticImageConfiguration(&testImage, getTestInitializerCore(), getTestCheckerCore())
+	config4 := builder.AddStaticImageConfiguration(&testImage, getTestInitializerCore("test4"), getTestCheckerCore())
 	idSet[config4] = true
-	config5 := builder.AddTestImageConfiguration(getTestInitializerCore(), getTestCheckerCore())
+	config5 := builder.AddTestImageConfiguration(getTestInitializerCore("test5"), getTestCheckerCore())
 	idSet[config5] = true
 	assert.Assert(t, len(idSet) == 5, "IDs should be different.")
 }
 
 func TestIdsDifferent(t *testing.T) {
 	builder := NewServiceNetworkConfigBuilder()
-	config := builder.AddTestImageConfiguration(getTestInitializerCore(), getTestCheckerCore())
+	config := builder.AddTestImageConfiguration(getTestInitializerCore("test"), getTestCheckerCore())
 	svc1, err := builder.AddService(config, 1, make(map[int]bool))
 	if err != nil {
 		t.Fatal("Add service shouldn't return error here")
@@ -108,7 +141,7 @@ func TestIdsDifferent(t *testing.T) {
 
 func TestDependencyBookkeeping(t *testing.T) {
 	builder := NewServiceNetworkConfigBuilder()
-	config := builder.AddTestImageConfiguration(getTestInitializerCore(), getTestCheckerCore())
+	config := builder.AddTestImageConfiguration(getTestInitializerCore("test"), getTestCheckerCore())
 
 	svc1, err := builder.AddService(config, 1, make(map[int]bool))
 	if err != nil {
@@ -174,7 +207,7 @@ func TestDependencyBookkeeping(t *testing.T) {
 
 func TestDefensiveCopies(t *testing.T) {
 	builder := NewServiceNetworkConfigBuilder()
-	config := builder.AddTestImageConfiguration(getTestInitializerCore(), getTestCheckerCore())
+	config := builder.AddTestImageConfiguration(getTestInitializerCore("test1"), getTestCheckerCore())
 
 	dependencyMap := make(map[int]bool)
 	svc1, err := builder.AddService(config, 1, dependencyMap)
@@ -184,7 +217,7 @@ func TestDefensiveCopies(t *testing.T) {
 
 	networkConfig := builder.Build()
 
-	_ = builder.AddTestImageConfiguration(getTestInitializerCore(), getTestCheckerCore())
+	_ = builder.AddTestImageConfiguration(getTestInitializerCore("test2"), getTestCheckerCore())
 	_, err = builder.AddService(config, 2, make(map[int]bool))
 	if err != nil {
 		t.Fatal("Add service shouldn't return error here")
@@ -200,4 +233,50 @@ func TestDefensiveCopies(t *testing.T) {
 	assert.Equal(t, 0, len(svcDependencies[svc1]))
 
 	// TODO test that the dependencies in the GetStartCommand are what we expect!
+}
+
+func TestDependencyFeeding(t *testing.T) {
+	builder := NewServiceNetworkConfigBuilder()
+	service1Result := "test1"
+	service1Initializer := getTestInitializerCore(service1Result)
+	config1 := builder.AddTestImageConfiguration(service1Initializer, getTestCheckerCore())
+	service2Result := "test2"
+	service2Initializer := getTestInitializerCore(service2Result)
+	config2 := builder.AddTestImageConfiguration(service2Initializer, getTestCheckerCore())
+	service3Result := "test3"
+	service3Initializer := getTestInitializerCore(service3Result)
+	config3 := builder.AddTestImageConfiguration(service3Initializer, getTestCheckerCore())
+
+	_, err := builder.AddService(config1, 1, make(map[int]bool))
+	if err != nil {
+		t.Fatal("Add service shouldn't return error here")
+	}
+
+	_, err = builder.AddService(config2, 2, map[int]bool{1: true})
+	if err != nil {
+		t.Fatal("Add service shouldn't return error here")
+	}
+	_, err = builder.AddService(config3, 3, map[int]bool{1: true, 2: true})
+	if err != nil {
+		t.Fatal("Add service shouldn't return error here")
+	}
+	networkConfig := builder.Build()
+	networkConfig.CreateNetwork("testImage", )
+
+
+	assert.Equal(t, 0, len(service1Initializer.GetLastDependencyResults()))
+	service2DepResults := service2Initializer.GetLastDependencyResults()
+	assert.Equal(t, 1, len(service2DepResults))
+	if _, found := service2DepResults[service1Result]; !found {
+		t.Fatalf("Should have found %v in service 2 dep results but it was missing", service1Result)
+	}
+
+	service3DepResults := service3Initializer.GetLastDependencyResults()
+	assert.Equal(t, 2, len(service3DepResults))
+	if _, found := service3DepResults[service1Result]; !found {
+		t.Fatalf("Should have found %v in service 3 dep results but it was missing", service1Result)
+	}
+	if _, found := service3DepResults[service2Result]; !found {
+		t.Fatalf("Should have found %v in service 3 dep results but it was missing", service2Result)
+	}
 }
